ab2162817be00cb5c96cabba83d6597cb5f9e85d
gitstorm1 <56407595+gitstorm1@users.noreply.github.com>
2025-12-22 11:02:08 +0500

    Adjustments to config

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,16 @@
 import express, { type Request, type Response } from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 
-dotenv.config();
-
 const app = express();
-const PORT = +(process.env.PORT || 5000);
+const PORT = +(process.env["PORT"] || 5000);
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Coldbook API is running... 🚀");
+    res.send("Coldbook API is running...");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is sprinting on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
