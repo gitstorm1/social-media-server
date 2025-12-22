93876fdbff87ff59a30b4de1aa4a3d6d3fb2e9c5
gitstorm1 <56407595+gitstorm1@users.noreply.github.com>
2025-12-22 12:27:30 +0500

    Begin basic migrations setup

---
+++ migrations/1766388154282_init-users-table.ts
@@ -0,0 +1,11 @@
+import { ColumnDefinitions, MigrationBuilder } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+    pgm.sql(`
+        
+    `);
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {}
---
+++ src/config/db.ts
@@ -0,0 +1,11 @@
+import pg from "pg";
+const { Pool } = pg;
+
+const isProduction = process.env["NODE_ENV"] === 'production';
+
+export const pool = new Pool({
+    connectionString: process.env["DATABASE_URL"],
+    ssl: isProduction
+        ? { rejectUnauthorized: false }
+        : false // Disables SSL for localhost
+});
