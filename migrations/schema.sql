04d1760f91b869a678ff44a6079f6ca9b97a87c2
gitstorm1 <56407595+gitstorm1@users.noreply.github.com>
2025-12-22 14:05:49 +0500

    Schema almost complete (but untested yet)

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -1,3 +1,6 @@
+BEGIN;
+
+-- EXTENSIONS & FUNCTIONS
 CREATE EXTENSION IF NOT EXISTS citext;
 
 CREATE OR REPLACE FUNCTION update_timestamp()
@@ -8,7 +11,8 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TABLE users (
+-- USERS TABLE
+CREATE TABLE IF NOT EXISTS users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 
     first_name TEXT,
@@ -31,10 +35,58 @@ CREATE TABLE users (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP(0)
 );
 
-CREATE INDEX idx_users_full_name ON users (first_name, last_name);
+CREATE INDEX IF NOT EXISTS idx_users_full_name ON users (first_name, last_name);
 
-
+DROP TRIGGER IF EXISTS trg_users_updated_at ON users;
 CREATE TRIGGER trg_users_updated_at
 BEFORE UPDATE ON users
 FOR EACH ROW
 EXECUTE FUNCTION update_timestamp();
+
+-- POSTS TABLE
+CREATE TABLE IF NOT EXISTS posts (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    content TEXT NOT NULL CHECK (char_length(content) BETWEEN 1 AND 2800),
+    
+    -- Foreign key (with Cascade delete)
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP(0)
+);
+
+CREATE INDEX IF NOT EXISTS idx_posts_user_feed 
+ON posts(user_id) 
+INCLUDE (created_at);
+
+DROP TRIGGER IF EXISTS trg_posts_updated_at ON posts;
+CREATE TRIGGER trg_posts_updated_at
+BEFORE UPDATE ON posts
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
+
+-- COMMENTS TABLE
+CREATE TABLE IF NOT EXISTS comments (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    post_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    content TEXT NOT NULL CHECK (char_length(content) <= 500),
+    
+    -- Foreign keys (with Cascade delete)
+    CONSTRAINT fk_post FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    CONSTRAINT fk_commenter FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP(0)
+);
+
+CREATE INDEX IF NOT EXISTS idx_comments_post_id ON comments(post_id);
+
+DROP TRIGGER IF EXISTS trg_comments_updated_at ON comments;
+CREATE TRIGGER trg_comments_updated_at
+BEFORE UPDATE ON comments
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
+
+COMMIT;
