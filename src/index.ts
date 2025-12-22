db2d4343536df1be656917b7d27e037ba47969ee
gitstorm1 <56407595+gitstorm1@users.noreply.github.com>
2025-12-22 10:52:42 +0500

    Set up basic structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("test");
+import express, { type Request, type Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = +(process.env.PORT || 5000);
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Coldbook API is running... 🚀");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is sprinting on port ${PORT}`);
+});
